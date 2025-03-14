3862650c14fa00c9df0a251efa1da409722e9634
Hitz <16.leminhnguyen@gmail.com>
2025-03-14 04:14:09 +0000

    Remove Collect-Metrics subproject and associated test script

--- test/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source "$(dirname "$0")/../.env"
-echo "  agents = [\"udp://$TELEGRAF_SNMP_HOST:161\"]" >> "./telegraf.conf"
-echo "$TELEGRAF_CONFIG_DIR"
-echo "  community = \"$TELEGRAF_SNMP_COMMUNITY\""
